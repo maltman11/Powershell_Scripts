3f603d3c842f253d55245af87b87103602d115cf
maltman11 <matt11380@hotmail.com>
2019-06-04 10:06:37 -0500

    Update WinFirewall.ps1

--- WinFirewall.ps1
+++ WinFirewall.ps1
@@ -1,3 +1,7 @@
 ﻿New-NetFirewallRule -DisplayName 'Port 5985 Inbound' -Name 'Port 5985 Inbound' -Direction Inbound -LocalPort '5985' -Protocol TCP -Action Allow
  
 New-NetFirewallRule -DisplayName 'Port 5985 Outbound' -Name 'Port 5985 Outbound' -Direction Outbound -LocalPort '5985' -Protocol TCP -Action Allow
+
+﻿New-NetFirewallRule -DisplayName 'Port 5986 Inbound' -Name 'Port 5985 Inbound' -Direction Inbound -LocalPort '5986' -Protocol TCP -Action Allow
+ 
+New-NetFirewallRule -DisplayName 'Port 59856 Outbound' -Name 'Port 5985 Outbound' -Direction Outbound -LocalPort '5986' -Protocol TCP -Action Allow
